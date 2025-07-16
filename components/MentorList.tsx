defd87f125bbc346cced1e4db02845928d4f3fc7
Heryan Djaruma <heryandjaruma@gmail.com>
2025-07-16 21:35:24 +0700

    minor layouting

--- components/MentorList.tsx
+++ components/MentorList.tsx
@@ -19,13 +19,13 @@ export default function MentorListComponent() {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-xl font-semibold">All Mentors</h1>
-      <div className="rounded-xl p-4 grid grid-cols-1 md:grid-cols-2">
+      <div className="rounded-xl p-4 grid grid-cols-1 md:grid-cols-2 gap-4">
         {mentors?.map((m) => (
           <div key={m.id} className="border rounded-xl border-gray-400 p-4 flex justify-between">
             <div>
               <p className="font-semibold">{m.name}</p>
               <p className="text-muted-foreground">{m.email}</p>
-              <p className="">Specialization: {m.specialization}</p>
+              <p className="mt-1">{m.specialization.toUpperCase()}</p>
             </div>
             <button className="font-semibold text-sm hover:underline">
               View
