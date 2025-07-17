643e2775c7b5b8118cf295c33bbf167343e3db1e
Heryan Djaruma <heryandjaruma@gmail.com>
2025-07-17 15:58:40 +0700

    remove unused code

--- components/MentorItemComponent.tsx
+++ components/MentorItemComponent.tsx
@@ -9,19 +9,6 @@ export default function MentorItemComponent({ mentor: m }: { mentor: FirestoreMe
         <p className="font-semibold">{m.name}</p>
         <p className="text-muted-foreground">{m.email}</p>
         <p className="">{m.specialization.toUpperCase()}</p>
-
-        {m.availableMentorings && (
-          <div className="">
-            {m.availableMentorings.map((availableMentoring, index) => {
-              return (
-                <div key={index} className="text-sm flex gap-2 px-2 border bg-muted rounded-xl">
-                  <span>{epochToStringDate(availableMentoring.startTime)} WIB - {epochToStringDate(availableMentoring.endTime)} WIB</span>
-                </div>
-              )
-            }
-            )}
-          </div>
-        )}
       </div>
       <button className="font-semibold text-sm hover:underline">
         View
